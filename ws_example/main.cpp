1d29082f8b8d7570c0221a3da87d3d8448e115d0
Grimmer <grimmer0125@gmail.com>
2017-07-04 15:55:06 +0800

    Initial Commit

---
+++ ws_example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ws_example
+//
+//  Created by Grimmer Kang on 2017/7/4.
+//  Copyright © 2017年 Grimmer Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
