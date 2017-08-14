f9d127747939a21a18c7537cd644c57f942ca714
Grimmer <grimmer0125@gmail.com>
2017-08-14 15:51:31 +0800

    Let Websocket server work in std::thread.

--- testws/main.cpp
+++ testws/main.cpp
@@ -3,27 +3,33 @@
 #include <QDebug>
 
 #include "uWS.h"
+//#include <uWS/uWS.h> not this?
+
+#include <thread>
+
+using namespace std;
+
+void testWebSocket(){
+    uWS::Hub h;
+    h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+        std::cout << "get something, echo back" << std::endl;
+        ws->send(message, length, opCode);
+    });
+    h.listen(3003);
+    h.run();
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     qDebug() << "start test websocket";
-//    std::cout << "get something, echo back" << std::endl;
+    std::cout << "get something2, echo back" << std::endl;
 
-    uWS::Hub h;
-
-    h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
-        std::cout << "get something, echo back" << std::endl;
-
-        ws->send(message, length, opCode);
-
-    });
-
-    h.listen(3003);
-    h.run();
+    thread mThread( testWebSocket );
 
     qDebug() << "end to test websocket";
 
+    mThread.detach();
     return a.exec(); // will block at here since QCoreApplication's event loop
 }
